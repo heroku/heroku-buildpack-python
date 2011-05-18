ad8bebe048c184deada6cd3683a2ef74de78b429
Noah Zoschke <noah@heroku.com>
2011-05-18 09:10:04 -0700

    dont remove .git to allow testing afterwards

--- test/canary_django/deploy.sh
+++ test/canary_django/deploy.sh
@@ -8,6 +8,5 @@ git add . && git commit -m 'Django skeleton'
 heroku create canary-django --stack cedar
 heroku config:add LANGUAGE_PACK_URL=https://${GITHUB_AUTH}@github.com/heroku/language-pack-python.git --app canary-django
 git push heroku master
-rm -rf .git
 
 curl --head http://canary-django.herokuapp.com/
