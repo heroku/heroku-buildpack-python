3e55317ff088a2225a4468f8b2c7bc1872c8776c
Noah Zoschke <noah@heroku.com>
2011-05-18 09:06:18 -0700

    require full github opts

--- test/canary_django/deploy.sh
+++ test/canary_django/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-[[ $GIT_PASSWORD = "" ]] && { echo "usage: GIT_PASSWORD=xxx ./push.sh"; exit 1; }
+[[ $GITHUB_AUTH = "" ]] && { echo "usage: GITHUB_AUTH=user:pass ./push.sh"; exit 1; }
 heroku destroy --app canary-django --confirm canary-django
 
 rm -rf .git
 git init .
 git add . && git commit -m 'Django skeleton'
 heroku create canary-django --stack cedar
-heroku config:add LANGUAGE_PACK_URL=https://nzoschke:${GIT_PASSWORD}@github.com/heroku/language-pack-python.git --app canary-django
+heroku config:add LANGUAGE_PACK_URL=https://${GITHUB_AUTH}@github.com/heroku/language-pack-python.git --app canary-django
 git push heroku master
 rm -rf .git
 
