7c877362e43439692ae610996eb644cefb590a1b
Aaron Triantafyllidis <atriantafyllidis@pivotallabs.com>
2014-05-13 17:03:39 -0400

    add extra deps for mercurial requirements app, fix test

--- cf_spec/integration/warn_on_mercurial_spec.rb
+++ cf_spec/integration/warn_on_mercurial_spec.rb
@@ -6,8 +6,8 @@ describe 'Mercurial Pip dependencies' do
   if Machete::BuildpackMode.offline?
     specify do
       Machete.deploy_app('mercurial_requirements', :python) do |app|
-        expect(app).to be_staged
-        expect(app.staging_log).to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
+        expect(app).not_to be_staged
+        expect(app.output).to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
       end
     end
   end
