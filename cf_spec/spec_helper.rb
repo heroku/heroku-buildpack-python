bac34b8f8c0c2dbd5f9c15872685b75e61f09eca
Aaron Triantafyllidis <atriantafyllidis@pivotallabs.com>
2014-05-13 16:21:14 -0400

    Make tests log to file

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'bundler/setup'
 require 'machete'
 
+`mkdir -p log`
+Machete.logger = Machete::Logger.new("log/integration.log")
+
 RSpec.configure do |config|
   config.before(:suite) do
     Machete::BuildpackUploader.new(:python)
