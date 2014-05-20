875b6c94cfb87b630d5b513c89106d7de6564d6d
Jordi Noguera <jordi@pivotallabs.com>
2014-05-20 16:46:31 -0400

    use base packager gem

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 
-gem 'rubyzip'
+gem 'base_packager', git: "https://github.com/cf-buildpacks/base_packager.git", ref: "317df64"
--- lib/buildpack_packager.rb
+++ lib/buildpack_packager.rb
@@ -1,71 +1,28 @@
-require 'zip'
-require 'tmpdir'
+require 'base_packager'
 require 'json'
 
-module BuildpackPackager
-  EXCLUDE_FROM_BUILDPACK = [
-      /\.git/,
-      /\.gitignore/,
-      /\.{1,2}$/
-  ]
+class BuildpackPackager < BasePackager
 
-  DEPENDENCIES = [
-    "http://envy-versions.s3.amazonaws.com/python-2.7.0.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-2.7.1.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-2.7.2.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-2.7.3.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-2.7.6.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-3.2.0.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-3.2.1.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-3.2.2.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-3.2.3.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/python-3.4.0.tar.bz2",
-    "http://envy-versions.s3.amazonaws.com/pypy-1.9.tar.bz2",
-    "http://cl.ly/0a191R3K160t1w1P0N25/vendor-libmemcached.tar.gz"
-  ]
+  def dependencies
+    [
+      "http://envy-versions.s3.amazonaws.com/python-2.7.0.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-2.7.1.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-2.7.2.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-2.7.3.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-2.7.6.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-3.2.0.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-3.2.1.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-3.2.2.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-3.2.3.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/python-3.4.0.tar.bz2",
+      "http://envy-versions.s3.amazonaws.com/pypy-1.9.tar.bz2",
+      "http://cl.ly/0a191R3K160t1w1P0N25/vendor-libmemcached.tar.gz"
+    ]
+  end
 
-  class << self
-    def package(mode)
-      Dir.mktmpdir do |temp_dir|
-        copy_buildpack_contents(temp_dir)
-        download_dependencies(temp_dir) if mode == :offline
-        compress_buildpack(temp_dir)
-      end
-    end
-
-    private
-
-    def copy_buildpack_contents(target_path)
-      run_cmd "cp -r * #{target_path}"
-    end
-
-    def download_dependencies(target_path)
-      dependency_path = File.join(target_path, 'dependencies')
-
-      run_cmd "mkdir -p #{dependency_path}"
-
-      DEPENDENCIES.each do |uri|
-        run_cmd "cd #{dependency_path}; curl #{uri} -O -L"
-      end
-    end
-
-    def in_pack?(file)
-      !EXCLUDE_FROM_BUILDPACK.any? { |re| file =~ re }
-    end
-
-    def compress_buildpack(target_path)
-      Zip::File.open('python_buildpack.zip', Zip::File::CREATE) do |zipfile|
-        Dir.glob(File.join(target_path, "**", "**"), File::FNM_DOTMATCH).each do |file|
-          zipfile.add(file.sub(target_path + '/', ''), file) if (in_pack?(file))
-        end
-      end
-    end
-
-    def run_cmd(cmd)
-      puts "$ #{cmd}"
-      system "#{cmd}"
-    end
+  def excluded_files
+    []
   end
 end
 
-BuildpackPackager.package(ARGV.first.to_sym)
+BuildpackPackager.new("python", ARGV.first.to_sym).package
