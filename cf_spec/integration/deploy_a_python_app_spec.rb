f37cf08caa34f4d841a55c686e816b2103d1cda1
Aaron Triantafyllidis <atriantafyllidis@pivotallabs.com>
2014-05-23 15:28:00 -0400

    remove test that checks mercurial warning in online mode

--- cf_spec/integration/deploy_a_python_app_spec.rb
+++ cf_spec/integration/deploy_a_python_app_spec.rb
@@ -2,19 +2,17 @@ $: << 'cf_spec'
 require "spec_helper"
 
 describe 'deploying a python web app' do
-  it "makes the homepage available" do
+  it "makes the homepage available for a flask web app" do
     Machete.deploy_app("flask_web_app", :python) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "Hello, World!"
     end
   end
 
-  if Machete::BuildpackMode.online?
-    it "makes the homepage available" do
-      Machete.deploy_app("flask_web_app_no_dependencies", :python) do |app|
-        expect(app).to be_staged
-        expect(app.homepage_html).to include "Hello, World!"
-      end
+  it "makes the homepage available for a flask web app without dependencies", if: Machete::BuildpackMode.online? do
+    Machete.deploy_app("flask_web_app_no_dependencies", :python) do |app|
+      expect(app).to be_staged
+      expect(app.homepage_html).to include "Hello, World!"
     end
   end
 end
--- cf_spec/integration/warn_on_mercurial_spec.rb
+++ cf_spec/integration/warn_on_mercurial_spec.rb
@@ -2,22 +2,10 @@ $: << 'cf_spec'
 require "spec_helper"
 
 describe 'Mercurial Pip dependencies' do
-
-  if Machete::BuildpackMode.offline?
-    specify do
-      Machete.deploy_app('mercurial_requirements', :python) do |app|
-        expect(app).not_to be_staged
-        expect(app.output).to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
-      end
-    end
-  end
-
-  if Machete::BuildpackMode.online?
-    specify do
-      Machete.deploy_app('mercurial_requirements', :python) do |app|
-        expect(app).to be_staged
-        expect(app.staging_log).not_to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
-      end
+  it "prints mercurial warning", if: Machete::BuildpackMode.offline? do
+    Machete.deploy_app('mercurial_requirements', :python) do |app|
+      expect(app).not_to be_staged
+      expect(app.output).to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
     end
   end
 end
