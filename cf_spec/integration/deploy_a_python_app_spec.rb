dfa2e13ac35825f1757d0a3b3d79016cf1ca8e70
Aaron Triantafyllidis <atriantafyllidis@pivotallabs.com>
2014-05-27 11:36:38 -0400

    rename flask app to reflect that it has no vendoring

--- cf_spec/integration/deploy_a_python_app_spec.rb
+++ cf_spec/integration/deploy_a_python_app_spec.rb
@@ -2,15 +2,15 @@ $: << 'cf_spec'
 require "spec_helper"
 
 describe 'deploying a python web app' do
-  it "makes the homepage available for a flask web app" do
+  it "successfully deploys apps with vendored dependencies" do
     Machete.deploy_app("flask_web_app", :python) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "Hello, World!"
     end
   end
 
-  it "makes the homepage available for a flask web app without dependencies", if: Machete::BuildpackMode.online? do
-    Machete.deploy_app("flask_web_app_no_dependencies", :python) do |app|
+  it "successfully deploys apps without vendored depdendencies", if: Machete::BuildpackMode.online? do
+    Machete.deploy_app("flask_web_app_not_vendored", :python) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "Hello, World!"
     end
