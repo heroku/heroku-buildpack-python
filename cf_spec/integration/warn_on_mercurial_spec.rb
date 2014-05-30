ac59aadfbaa67ee44bb33d7dc1ca281b7f2c17c7
Aaron Triantafyllidis <atriantafyllidis@pivotallabs.com>
2014-05-30 11:14:09 -0400

    Mercurial spec now checks logs instead of output

--- cf_spec/integration/warn_on_mercurial_spec.rb
+++ cf_spec/integration/warn_on_mercurial_spec.rb
@@ -5,7 +5,7 @@ describe 'Mercurial Pip dependencies' do
   it "prints mercurial warning", if: Machete::BuildpackMode.offline? do
     Machete.deploy_app('mercurial_requirements', :python) do |app|
       expect(app).not_to be_staged
-      expect(app.output).to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
+      expect(app.logs).to include 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
     end
   end
 end
