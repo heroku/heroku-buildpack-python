ad8bc84e652caad7de1aa5ec732c6ac9c06e8366
Aaron Triantafyllidis and Tim Johnson <pair+atriantafyllidis+buddhistpirate@pivotallabs.com>
2014-06-18 09:32:35 -0400

    Cache mode does not try to reach the internet
    [#73420832]

--- cf.Gemfile
+++ cf.Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 
-gem 'machete', git: 'https://github.com/cf-buildpacks/machete.git', ref: 'a73b3c23dc06387a8500aa8c691a43623afe28e3'
+gem 'machete', github: 'cf-buildpacks/machete', branch: 'master'
--- cf_spec/integration/deploy_a_python_app_spec.rb
+++ cf_spec/integration/deploy_a_python_app_spec.rb
@@ -1,18 +1,43 @@
 $: << 'cf_spec'
-require "spec_helper"
+require 'spec_helper'
 
-describe 'deploying a python web app' do
-  it "successfully deploys apps with vendored dependencies" do
-    Machete.deploy_app("flask_web_app") do |app|
-      expect(app).to be_staged
-      expect(app.homepage_html).to include "Hello, World!"
+describe 'CF Python Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name) }
+
+  context 'with cached buildpack dependencies' do
+    context 'in an offline environment', if: Machete::BuildpackMode.offline? do
+      context 'app has dependencies' do
+        let(:app_name) { 'flask_web_app' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'Hello, World!'
+          expect(app).to have_no_internet_traffic
+        end
+      end
     end
   end
 
-  it "successfully deploys apps without vendored depdendencies", if: Machete::BuildpackMode.online? do
-    Machete.deploy_app("flask_web_app_not_vendored") do |app|
-      expect(app).to be_staged
-      expect(app.homepage_html).to include "Hello, World!"
+  context 'without cached buildpack dependencies' do
+    context 'in an online environment', if: Machete::BuildpackMode.online? do
+
+      context 'app has dependencies' do
+        let(:app_name) { 'flask_web_app' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'Hello, World!'
+        end
+      end
+
+      context 'app has no dependencies' do
+        let(:app_name) { 'flask_web_app_not_vendored' }
+
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'Hello, World!'
+        end
+      end
     end
   end
 end
