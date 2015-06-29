8b10c937eb5ebd41f49d2d6039b64872233c1000
Colin Jackson <colin.s.jackson90@gmail.com>
2015-06-29 15:51:03 -0400

    Bump machete to v0.0.17
    - This version uses more accurate language to refer to buildpack modes
      (cached/uncached)
    
    [#90408666]
    
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- cf.Gemfile
+++ cf.Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 ruby '2.1.5'
 
-gem 'machete', git: 'https://github.com/cf-buildpacks/machete', tag: 'v0.0.12'
+gem 'machete', git: 'https://github.com/cf-buildpacks/machete', tag: 'v0.0.17'
 gem 'buildpack-packager', git: 'https://github.com/cloudfoundry-incubator/buildpack-packager', tag: 'v2.2.0'
 gem 'rspec'
 gem 'rspec-instafail'
--- cf_spec/integration/deploy_a_python_app_spec.rb
+++ cf_spec/integration/deploy_a_python_app_spec.rb
@@ -4,82 +4,55 @@ describe 'CF Python Buildpack' do
   subject(:app) { Machete.deploy_app(app_name) }
   let(:browser) { Machete::Browser.new(app) }
 
-  context 'with cached buildpack dependencies' do
-    context 'in an offline environment', if: Machete::BuildpackMode.offline? do
-      context 'app has dependencies' do
-        context 'with Python 2' do
-          let(:app_name) { 'flask_web_app' }
+  context 'with cached buildpack dependencies', if: Machete::BuildpackMode.cached? do
+    context 'app has dependencies' do
+      context 'with Python 2' do
+        let(:app_name) { 'flask_web_app' }
 
-          specify do
-            expect(app).to be_running(60)
+        specify do
+          expect(app).to be_running(60)
 
-            browser.visit_path('/')
-            expect(browser).to have_body('Hello, World!')
+          browser.visit_path('/')
+          expect(browser).to have_body('Hello, World!')
 
-            expect(app.host).not_to have_internet_traffic
-          end
-        end
-
-        context 'with Python 3' do
-          let(:app_name) { 'flask_web_app_python_3' }
-
-          specify do
-            expect(app).to be_running(60)
-
-            browser.visit_path('/')
-            expect(browser).to have_body('Hello, World!')
-
-            expect(app.host).not_to have_internet_traffic
-          end
+          expect(app.host).not_to have_internet_traffic
         end
       end
 
-      context 'Warning when pip has mercurial dependencies' do
-        let(:log_file) { File.open('log/integration.log', 'r') }
-        let(:app_name) { 'mercurial_requirements' }
-
-        before do
-          log_file.readlines
-        end
+      context 'with Python 3' do
+        let(:app_name) { 'flask_web_app_python_3' }
 
         specify do
-          expect(app).not_to be_running(0)
-          statement_appearances = log_file.readlines.join.scan 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
-          expect(statement_appearances.count).to be 1
+          expect(app).to be_running(60)
+
+          browser.visit_path('/')
+          expect(browser).to have_body('Hello, World!')
+
+          expect(app.host).not_to have_internet_traffic
         end
+      end
+    end
+
+    context 'Warning when pip has mercurial dependencies' do
+      let(:log_file) { File.open('log/integration.log', 'r') }
+      let(:app_name) { 'mercurial_requirements' }
+
+      before do
+        log_file.readlines
+      end
+
+      specify do
+        expect(app).not_to be_running(0)
+        statement_appearances = log_file.readlines.join.scan 'Cloud Foundry does not support Pip Mercurial dependencies while in offline-mode. Vendor your dependencies if they do not work.'
+        expect(statement_appearances.count).to be 1
       end
     end
   end
 
-  context 'without cached buildpack dependencies' do
-    context 'in an online environment', if: Machete::BuildpackMode.online? do
-
-      context 'app has dependencies' do
-        context 'with Python 2' do
-          let(:app_name) { 'flask_web_app' }
-
-          specify do
-            expect(app).to be_running(60)
-
-            browser.visit_path('/')
-            expect(browser).to have_body('Hello, World!')
-          end
-        end
-
-        context 'with Python 3' do
-          let(:app_name) { 'flask_web_app_python_3' }
-
-          specify do
-            expect(app).to be_running(60)
-
-            browser.visit_path('/')
-            expect(browser).to have_body('Hello, World!')
-          end
-        end
-      end
-
-      context 'app has no dependencies' do
-        let(:app_name) { 'flask_web_app_not_vendored' }
+  context 'without cached buildpack dependencies', if: Machete::BuildpackMode.uncached? do
+    context 'app has dependencies' do
+      context 'with Python 2' do
+        let(:app_name) { 'flask_web_app' }
 
         specify do
           expect(app).to be_running(60)
@@ -87,6 +60,28 @@ describe 'CF Python Buildpack' do
           browser.visit_path('/')
           expect(browser).to have_body('Hello, World!')
         end
+      end
+
+      context 'with Python 3' do
+        let(:app_name) { 'flask_web_app_python_3' }
+
+        specify do
+          expect(app).to be_running(60)
+
+          browser.visit_path('/')
+          expect(browser).to have_body('Hello, World!')
+        end
+      end
+    end
+
+    context 'app has no dependencies' do
+      let(:app_name) { 'flask_web_app_not_vendored' }
+
+      specify do
+        expect(app).to be_running(60)
+
+        browser.visit_path('/')
+        expect(browser).to have_body('Hello, World!')
       end
     end
   end
