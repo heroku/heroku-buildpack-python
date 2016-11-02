31b516bae59f01368c38775c141e9686fba55bdc
Jacques Chester <jchester@pivotal.io>
2016-11-02 13:50:14 -0400

    Make RSpec emit colored output no matter what
    We thought concourse-filter was swallowing color codes.
    
    It was not. Instead, they were not emitted on CI.
    
    [#133046105]

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -6,6 +6,9 @@ require 'machete/matchers'
 Machete.logger = Machete::Logger.new("log/integration.log")
 
 RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+
   config.filter_run_excluding :cached => ENV['BUILDPACK_MODE'] == 'uncached'
   config.filter_run_excluding :uncached => ENV['BUILDPACK_MODE'] == 'cached'
 end
