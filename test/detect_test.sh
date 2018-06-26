b553654a2df0635a20f99f9a6ed383ed81eacc47
Cascode6 <caseylfaist@gmail.com>
2018-06-26 17:47:54 -0400

    get output of test

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -2,15 +2,10 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-testVersion()
-{
-  mkdir ${BUILD_DIR}/bin
-  touch ${BUILD_DIR}/bin/detect
-  touch ${BUILD_DIR}/bin/compile
-  touch ${BUILD_DIR}/bin/release
-  mkdir ${BUILD_DIR}/test
-  
+testDetect()
+{ 
   detect
   assertCapturedSuccess
   assertCaptured "Python"
+  echo ${rtrn}
 }
