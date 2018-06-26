77395ee09a9aa5f36f0e657761d9fa28b474c200
Cascode6 <caseylfaist@gmail.com>
2018-06-26 17:42:42 -0400

    test the detect output

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -2,7 +2,7 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-testDetect()
+testVersion()
 {
   mkdir ${BUILD_DIR}/bin
   touch ${BUILD_DIR}/bin/detect
@@ -10,34 +10,7 @@ testDetect()
   touch ${BUILD_DIR}/bin/release
   mkdir ${BUILD_DIR}/test
   
-  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
-  
-  assertEquals 0 ${rtrn}
-  assertEquals "Buildpack Test" "$(cat ${STD_OUT})"
-  assertEquals "" "$(cat ${STD_ERR})"
-}
-
-testNoDetectMissingBuildpackScripts()
-{
-  mkdir ${BUILD_DIR}/test
-
-  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
- 
-  assertEquals 1 ${rtrn}
-  assertEquals "no" "$(cat ${STD_OUT})"
-  assertEquals "" "$(cat ${STD_ERR})"
-}
-
-testNoDetectMissingTestDir()
-{
-  mkdir ${BUILD_DIR}/bin
-  touch ${BUILD_DIR}/bin/detect
-  touch ${BUILD_DIR}/bin/complie
-  touch ${BUILD_DIR}/bin/release
-
-  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
- 
-  assertEquals 1 ${rtrn}
-  assertEquals "no" "$(cat ${STD_OUT})"
-  assertEquals "" "$(cat ${STD_ERR})"
+  detect
+  assertCapturedSuccess
+  assertCaptured "Python"
 }
