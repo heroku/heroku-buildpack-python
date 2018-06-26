abe0d9b286c86e78746e65f99f9b234642a16316
Cascode6 <caseylfaist@gmail.com>
2018-06-26 18:04:06 -0400

    add coverage for detect behavior

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -4,8 +4,41 @@
 
 testDetect()
 { 
-  detect
-  assertCapturedSuccess
-  assertCaptured "Python"
-  echo ${rtrn}
+    touch ${BUILD_DIR}/requirements.txt
+    touch ${BUILD_DIR}/setup.py
+    touch ${BUILD_DIR}/Pipfile
+    detect
+    assertCapturedSuccess
+    assertCaptured "Python"
+}
+
+testRequirements()
+{ 
+    touch ${BUILD_DIR}/requirements.txt
+    detect
+    assertCapturedSuccess
+    assertCaptured "Python"
+}
+
+testPipfile()
+{ 
+    
+    touch ${BUILD_DIR}/Pipfile
+    detect
+    assertCapturedSuccess
+    assertCaptured "Python"
+}
+
+testSetup()
+{ 
+    touch ${BUILD_DIR}/setup.py
+    detect
+    assertCapturedSuccess
+    assertCaptured "Python"
+}
+
+testMissingAnyVersionFile()
+{ 
+    detect
+    assertCapturedError 1 ${rtrn}
 }
