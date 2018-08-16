ce14e1edf8daf4fe5081b05cfc12896c596c2d0b
schneems <richard.schneeman+foo@gmail.com>
2018-08-16 13:34:05 -0500

    Integration tests with Hatchet

---
+++ spec/hatchet/python_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../spec_helper'
+
+describe "Python!!!!!!!!!!!" do
+  it "🐍" do
+    Hatchet::Runner.new('python-getting-started', stack: DEFAULT_STACK).deploy do |app|
+      expect(app.output).to           match(/Installing pip/)
+      expect(app.run('python -V')).to match(/3.6.6/)
+    end
+  end
+end
