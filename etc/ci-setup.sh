576def4cde81cdbc4cae4fb3e0004f076ff5b87b
Casey Faist <caseylfaist@gmail.com>
2018-10-04 10:22:55 -0500

    fix travis dependency blocker

---
+++ etc/ci-setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get -qq update
+sudo apt-get install software-properties-common -y
+curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://cli-assets.heroku.com/install-ubuntu.sh | sh
