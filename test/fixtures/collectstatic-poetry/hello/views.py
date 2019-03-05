d0b268818e5fe36b6ed88c70e31e8786ec6d83b9
AndreGuerra123 <guerraandre@hotmail.com>
2019-03-05 16:27:03 +0000

    Tested Poetry Feature

---
+++ test/fixtures/collectstatic-poetry/hello/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
