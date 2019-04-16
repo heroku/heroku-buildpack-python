34c796b2b44ed7c4d4b8f786ea12f525970d5ff0
Soulou <leo@unbekandt.eu>
2019-04-16 10:09:50 +0200

    Remove CF offline deployment of buildpack and use a remote local cache proxying the downloading of assets from S3

--- cf.Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-ruby '2.1.1'
-
-gem 'machete', github: 'cf-buildpacks/machete', branch: 'master'
--- cf_spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'bundler/setup'
-require 'machete'
-require 'machete/matchers'
-
-`mkdir -p log`
-Machete.logger = Machete::Logger.new("log/integration.log")
