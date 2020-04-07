cb914257a20789d57ea05a69a5457d396e545640
Casey Faist <caseylfaist@gmail.com>
2020-04-07 16:00:58 -0400

    Debug output of changes, clean up whitespace

--- spec/hatchet/python_spec.rb
+++ spec/hatchet/python_spec.rb
@@ -4,13 +4,13 @@ describe "Default Python Deploy" do
   it "🐍" do
     Hatchet::Runner.new('python-getting-started', stack: DEFAULT_STACK).deploy do |app|
       expect(app.output).to           match(/Installing pip/)
-      expect(app.run('python -V')).to match(/3.6.10/)
 
-
+      expect(app.output).to_not match("No such file or directory")
       expect(app.output).to_not match("Clearing cached dependencies")
       expect(app.output).to_not match("cp: cannot create regular file")
 
       # Redeploy
+      run!(%Q{echo "" >> requirements.txt})
       run!(%Q{echo "flask" >> requirements.txt})
       run!(%Q{git add . ; git commit --allow-empty -m next})
       app.push!
@@ -23,7 +23,7 @@ describe "Default Python Deploy" do
 
       # The cache should not clear with no changes
       expect(app.output).to_not match("Clearing cached dependencies")
-
+      expect(app.run('python -V')).to match(/3.7.3/)
     end
   end
 end
