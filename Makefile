heroku/heroku-buildpack-python

commit a334672a1a2814509f4bad06139786e497adbf3f

reorder things
Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

bin/steps/pipenv
@@ -31,6 +31,9 @@ if [ ! "$SKIP_PIPENV_INSTALL" ]; then
             # Measure that we're using Pipenv.
             mcount "tool.pipenv"
 
+            # Skip pip install, later.
+            export SKIP_PIP_INSTALL=1
+
             # Set PIP_EXTRA_INDEX_URL
             if [[ -r $ENV_DIR/PIP_EXTRA_INDEX_URL ]]; then
                 PIP_EXTRA_INDEX_URL="$(cat "$ENV_DIR/PIP_EXTRA_INDEX_URL")"
@@ -59,9 +62,6 @@ if [ ! "$SKIP_PIPENV_INSTALL" ]; then
                 puts-step "Installing test dependencies…"
                 /app/.heroku/python/bin/pipenv install --dev --system --deploy 2>&1 | cleanup | indent
             fi
-
-            # Skip pip install, later.
-            export SKIP_PIP_INSTALL=1
 
             # Pip freeze, for compatibility.
             pip freeze > requirements.txt

commit 2dce32c0e31fdde6862a1ccdb59788228f892904

Remove unused pip-uninstall step
The pip-uninstall step has been unused for pip-using apps since #925,
since the buildpack now invalidates the entire package cache instead.

Whilst the step appears to still be used for pipenv-using apps, the
code is not run, since `SKIP_PIP_INSTALL=1` is set too early.

This bug was introduced in a334672a1a2814509f4bad06139786e497adbf3f
which landed straight to `master` two days after the feature was
introduced in #650.

Longer term we should likely get pipenv installs to do something similar
to pip (invalidate the whole cache based on checksum of the lockfile),
however for now I'm removing this deadcode since it's the last consumer
of the `pip-diff` script which we want to remove.

Closes @W-8386830@.

Makefile
@@ -12,7 +12,7 @@ STACK_IMAGE_TAG := heroku/$(subst -,:,$(STACK))-build
 
 check:
 	@shellcheck -x bin/compile bin/detect bin/release bin/test-compile bin/utils bin/warnings bin/default_pythons
-	@shellcheck -x bin/steps/collectstatic bin/steps/eggpath-fix  bin/steps/eggpath-fix2 bin/steps/nltk bin/steps/pip-install bin/steps/pip-uninstall bin/steps/pipenv bin/steps/pipenv-python-version bin/steps/python
+	@shellcheck -x bin/steps/collectstatic bin/steps/eggpath-fix  bin/steps/eggpath-fix2 bin/steps/nltk bin/steps/pip-install bin/steps/pipenv bin/steps/pipenv-python-version bin/steps/python
 	@shellcheck -x bin/steps/hooks/*
 
 test:

bin/steps/pip-uninstall
@@ -1,28 +1,0 @@
-#!/usr/bin/env bash
-
-set +e
-# Install dependencies with Pip.
-# shellcheck source=bin/utils
-source "$BIN_DIR/utils"
-
-if [ ! "$SKIP_PIP_INSTALL" ]; then
-
-  if [[ -f .heroku/python/requirements-declared.txt ]]; then
-
-    cp .heroku/python/requirements-declared.txt requirements-declared.txt
-
-
-    if ! pip-diff --stale requirements-declared.txt requirements.txt --exclude setuptools pip wheel > .heroku/python/requirements-stale.txt; then
-      mcount "failure.bad-requirements"
-    fi
-
-    rm -rf requirements-declared.txt
-
-    if [[ -s .heroku/python/requirements-stale.txt ]]; then
-      puts-step "Uninstalling stale dependencies"
-      /app/.heroku/python/bin/pip uninstall -r .heroku/python/requirements-stale.txt -y --exists-action=w --disable-pip-version-check | cleanup | indent
-    fi
-  fi
-
-fi
-set -e

bin/steps/pipenv
@@ -69,7 +69,6 @@ if [ ! "$SKIP_PIPENV_INSTALL" ]; then
 
         else
             pipenv-to-pip Pipfile.lock > requirements.txt
-            "$BIN_DIR/steps/pip-uninstall"
             cp requirements.txt .heroku/python/requirements-declared.txt
             openssl dgst -sha256 Pipfile.lock > .heroku/python/Pipfile.lock.sha256
 