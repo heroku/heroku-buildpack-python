a1a9d64e185d10125bd1a8a8808eba75d89fba02
Ed Morley <501702+edmorley@users.noreply.github.com>
2021-01-25 12:21:48 +0000

    Tests: Cleanup of existing Hatchet tests
    Prior to the recent shunit2 to Hatchet test conversion, there were
    already a few Hatchet tests. This cleans those up slightly:
    * Makes `ci_spec.rb` use a fixture in the repository, instead of a
      mixture of remote repository (`sharpstone/python_default`) and
      content written out at test runtime.
    * Removes `sharpstone/python_default` from `hatchet.json` since there
      is nothing left using it.
    * Renames `python_spec.rb` to `getting_started_spec.rb` since that's
      the only test left in that file.
    
    Closes @W-8786787@.
    
    [skip changelog]

--- spec/hatchet/ci_spec.rb
+++ spec/hatchet/ci_spec.rb
@@ -4,25 +4,7 @@ require_relative '../spec_helper'
 
 RSpec.describe 'Heroku CI' do
   it 'works' do
-    before_deploy = proc do
-      File.open('app.json', 'w+') do |f|
-        f.puts <<~MANIFEST
-          {
-            "environments": {
-              "test": {
-                "scripts": {
-                  "test": "nosetests"
-                }
-              }
-            }
-          }
-        MANIFEST
-      end
-
-      run!('echo nose >> requirements.txt')
-    end
-
-    new_app('python_default', before_deploy: before_deploy).run_ci do |test_run|
+    new_app('spec/fixtures/ci_nose').run_ci do |test_run|
       expect(test_run.output).to match('Downloading nose')
       expect(test_run.output).to match('OK')
 
