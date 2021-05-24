a928a2ff5f87af9377a8804433e93aaf494f65f6
Ed Morley <501702+edmorley@users.noreply.github.com>
2021-05-24 09:39:43 +0100

    Fix parsing of DISABLE_COLLECTSTATIC env var values (#1208)
    By default if Django is installed and a `manage.py` file found, the
    buildpack will run Django's `collectstatic` command at the end of the
    build.
    
    This step can be skipped by setting `DISABLE_COLLECTSTATIC=1`:
    https://devcenter.heroku.com/articles/django-assets#disabling-collectstatic
    
    However previously, if `DISABLE_COLLECTSTATIC` was instead set to `0` or
    the empty string (such as when toggling it on and off during debugging),
    then collectstatic would still be skipped.
    
    This behaviour was made more confusing due to there being no build output
    that explained why collectstatic was being skipped.
    
    Now:
    * Using values of `0` or the empty string are treated the same as if
      `DISABLE_COLLECTSTATIC` was not set at all (all other values are
      unaffected).
    * If `collectstatic` is skipped (either due to that env var, the legacy
      skip file, or because a `manage.py` file is not found), then the build
      output now contains the reason why.
    * A deprecation warning is output when collectstatic is skipped via the
      legacy `.heroku/collectstatic_disabled` file method.
    
    I've also added some more tests around collectstatic, since there was
    only one before, and refactored the collectstatic step script to use
    early returns for improved readability.
    
    See:
    https://heroku.support/950668
    https://heroku.support/986549
    
    Closes GUS-W-8813947.

--- spec/hatchet/django_spec.rb
+++ spec/hatchet/django_spec.rb
@@ -3,41 +3,119 @@
 require_relative '../spec_helper'
 
 RSpec.describe 'Django support' do
-  context 'when requirements.txt contains Django==1.11' do
-    let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_1.11') }
+  describe 'Unsupported version warnings' do
+    context 'when requirements.txt contains Django==1.11' do
+      let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_1.11') }
 
-    it 'warns about Django end of support' do
-      app.deploy do |app|
-        expect(clean_output(app.output)).to include(<<~OUTPUT)
-          remote: -----> Installing requirements with pip
-          remote:  !     Your Django version is nearing the end of its community support.
-          remote:  !     Upgrade to continue to receive security updates and for the best experience with Django.
-          remote:  !     For more information, check out https://www.djangoproject.com/download/#supported-versions
-        OUTPUT
+      it 'warns about Django end of support' do
+        app.deploy do |app|
+          expect(clean_output(app.output)).to include(<<~OUTPUT)
+            remote: -----> Installing requirements with pip
+            remote:  !     Your Django version is nearing the end of its community support.
+            remote:  !     Upgrade to continue to receive security updates and for the best experience with Django.
+            remote:  !     For more information, check out https://www.djangoproject.com/download/#supported-versions
+          OUTPUT
+        end
+      end
+    end
+
+    context 'when requirements.txt contains Django==2.1' do
+      let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_2.1') }
+
+      it 'does not warn about Django end of support' do
+        app.deploy do |app|
+          expect(app.output).not_to include('https://www.djangoproject.com/download/#supported-versions')
+        end
       end
     end
   end
 
-  context 'when requirements.txt contains Django==2.1' do
-    let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_2.1') }
+  describe 'collectstatic' do
+    context 'when building a Django project' do
+      let(:app) { Hatchet::Runner.new('python-getting-started') }
 
-    it 'does not warn about Django end of support' do
-      app.deploy do |app|
-        expect(app.output).not_to include('https://www.djangoproject.com/download/#supported-versions')
+      it 'runs collectstatic' do
+        app.deploy do |app|
+          expect(clean_output(app.output)).to match(Regexp.new(<<~REGEX))
+            remote: -----> \\$ python manage.py collectstatic --noinput
+            remote:        \\d+ static files copied to '/tmp/build_.*/staticfiles', \\d+ post-processed.
+          REGEX
+        end
       end
     end
-  end
 
-  # TODO: Add tests for disabling collectstatic, failure cases etc.
-  context 'when building a Django project' do
-    let(:app) { Hatchet::Runner.new('python-getting-started') }
+    context 'when Django is installed but manage.py does not exist' do
+      let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_latest') }
 
-    it 'collectstatic is run automatically' do
-      app.deploy do |app|
-        expect(clean_output(app.output)).to match(Regexp.new(<<~REGEX))
-          remote: -----> \\$ python manage.py collectstatic --noinput
-          remote:        \\d+ static files copied to '/tmp/build_.*/staticfiles', \\d+ post-processed.
-        REGEX
+      it 'skips collectstatic' do
+        app.deploy do |app|
+          expect(app.output).to include('Skipping Django collectstatic since no manage.py file found.')
+          expect(app.output).not_to include('manage.py collectstatic')
+        end
+      end
+    end
+
+    context 'when DISABLE_COLLECTSTATIC=1' do
+      let(:app) do
+        Hatchet::Runner.new('spec/fixtures/requirements_django_latest', config: { 'DISABLE_COLLECTSTATIC' => '1' })
+      end
+
+      it 'skips collectstatic' do
+        app.deploy do |app|
+          expect(app.output).to include('Skipping Django collectstatic since the env var DISABLE_COLLECTSTATIC is set.')
+          expect(app.output).not_to include('static files copied')
+        end
+      end
+    end
+
+    context 'when DISABLE_COLLECTSTATIC=0' do
+      let(:app) { Hatchet::Runner.new('python-getting-started', config: { 'DISABLE_COLLECTSTATIC' => '0' }) }
+
+      it 'still runs collectstatic' do
+        app.deploy do |app|
+          expect(app.output).to include('static files copied')
+        end
+      end
+    end
+
+    context 'when DISABLE_COLLECTSTATIC is null' do
+      let(:app) { Hatchet::Runner.new('python-getting-started', config: { 'DISABLE_COLLECTSTATIC' => '' }) }
+
+      it 'still runs collectstatic' do
+        app.deploy do |app|
+          expect(app.output).to include('static files copied')
+        end
+      end
+    end
+
+    context 'when .heroku/collectstatic_disabled exists' do
+      let(:app) { Hatchet::Runner.new('spec/fixtures/django_collectstatic_disabled_file') }
+
+      it 'skips collectstatic with a deprecation warning' do
+        app.deploy do |app|
+          expect(clean_output(app.output)).to include(<<~OUTPUT)
+            remote: -----> Skipping Django collectstatic since the file '.heroku/collectstatic_disabled' exists.
+            remote:  !     This approach is deprecated, please set the env var DISABLE_COLLECTSTATIC=1 instead.
+          OUTPUT
+          expect(app.output).not_to include('static files copied')
+        end
+      end
+    end
+
+    context 'when building a broken Django project' do
+      let(:app) { Hatchet::Runner.new('spec/fixtures/django_broken_project', allow_failure: true) }
+
+      it 'fails collectstatic with an informative error message' do
+        app.deploy do |app|
+          expect(clean_output(app.output)).to match(Regexp.new(<<~REGEX, Regexp::MULTILINE))
+            remote: -----> \\$ python manage.py collectstatic --noinput
+            remote:        Traceback \\(most recent call last\\):
+            remote:        .*
+            remote:        ModuleNotFoundError: No module named 'gettingstarted'
+            remote:#{' '}
+            remote:  !     Error while running '\\$ python manage.py collectstatic --noinput'.
+          REGEX
+        end
       end
     end
   end
