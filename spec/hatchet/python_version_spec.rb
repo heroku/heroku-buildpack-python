555193827cae6c6ea60d45b180524bcb12776fdc
Ed Morley <501702+edmorley@users.noreply.github.com>
2021-11-16 10:55:42 +0000

    Add support for Python 3.9.9
    https://blog.python.org/2021/11/python-399-hotfix-release-is-now.html
    https://www.python.org/downloads/release/python-399/
    https://docs.python.org/release/3.9.9/whatsnew/changelog.html#python-3-9-9-final
    
    GUS-W-10182877.

--- spec/hatchet/python_version_spec.rb
+++ spec/hatchet/python_version_spec.rb
@@ -187,7 +187,7 @@ RSpec.describe 'Python version support' do
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_8
   end
 
-  context 'when runtime.txt contains python-3.9.8' do
+  context 'when runtime.txt contains python-3.9.9' do
     let(:app) { Hatchet::Runner.new('spec/fixtures/python_3.9') }
 
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_9
