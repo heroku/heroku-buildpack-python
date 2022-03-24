f336d107de5ed8bfa00000f9e61ae8b507b12aaf
Ed Morley <501702+edmorley@users.noreply.github.com>
2022-03-24 11:03:14 +0000

    Add support for Python 3.9.12 and 3.10.4
    https://www.python.org/downloads/release/python-3912/
    https://www.python.org/downloads/release/python-3104/
    https://docs.python.org/release/3.9.12/whatsnew/changelog.html#python-3-9-12-final
    https://docs.python.org/release/3.10.4/whatsnew/changelog.html#python-3-10-4-final
    
    GUS-W-10893598.
    GUS-W-10893600.

--- spec/hatchet/python_version_spec.rb
+++ spec/hatchet/python_version_spec.rb
@@ -187,13 +187,13 @@ RSpec.describe 'Python version support' do
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_8
   end
 
-  context 'when runtime.txt contains python-3.9.11' do
+  context 'when runtime.txt contains python-3.9.12' do
     let(:app) { Hatchet::Runner.new('spec/fixtures/python_3.9') }
 
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_9
   end
 
-  context 'when runtime.txt contains python-3.10.3' do
+  context 'when runtime.txt contains python-3.10.4' do
     let(:app) { Hatchet::Runner.new('spec/fixtures/python_3.10') }
 
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_10
