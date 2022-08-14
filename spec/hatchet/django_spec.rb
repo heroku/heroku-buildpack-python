d87b5d78a734cf511217b77959a7bd3adce61336
Ed Morley <501702+edmorley@users.noreply.github.com>
2022-08-14 10:46:13 +0100

    Remove outdated Django version warning
    Since:
    - the version check was pretty broken (only matches against
      that exact requirements file version syntax, plus doesn't
      check unpinned but still outdated version etc)
    - it only warned for Django 1.x, when there are lots of other
      EOL Django versions since then
    - it feels inconsistent for us to warn about one framework being
       EOL when there are lots of other packages and frameworks out
       there that should also be kept up to date too -- that's best handled
       by services like Dependabot security alerts etc
    
    If at such time in the future our Django collectstatic feature requires a
    specific version of Django, any version checking/warning should occur
    as part of that feature.
    
    GUS-W-11593026.

--- spec/hatchet/django_spec.rb
+++ spec/hatchet/django_spec.rb
@@ -3,33 +3,6 @@
 require_relative '../spec_helper'
 
 RSpec.describe 'Django support' do
-  describe 'Unsupported version warnings' do
-    context 'when requirements.txt contains Django==1.11' do
-      let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_1.11') }
-
-      it 'warns about Django end of support' do
-        app.deploy do |app|
-          expect(clean_output(app.output)).to include(<<~OUTPUT)
-            remote: -----> Installing requirements with pip
-            remote:  !     Your Django version is nearing the end of its community support.
-            remote:  !     Upgrade to continue to receive security updates and for the best experience with Django.
-            remote:  !     For more information, check out https://www.djangoproject.com/download/#supported-versions
-          OUTPUT
-        end
-      end
-    end
-
-    context 'when requirements.txt contains Django==2.1' do
-      let(:app) { Hatchet::Runner.new('spec/fixtures/requirements_django_2.1') }
-
-      it 'does not warn about Django end of support' do
-        app.deploy do |app|
-          expect(app.output).not_to include('https://www.djangoproject.com/download/#supported-versions')
-        end
-      end
-    end
-  end
-
   describe 'collectstatic' do
     context 'when building a Django project' do
       let(:app) { Hatchet::Runner.new('python-getting-started') }
