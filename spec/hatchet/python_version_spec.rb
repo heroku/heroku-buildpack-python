4dd34cd790fc591151995a2d2c1b050d29f52429
Ed Morley <501702+edmorley@users.noreply.github.com>
2024-04-09 16:54:52 +0000

    Add support for Python 3.12.3 (#1560)
    Release announcement:
    https://www.python.org/downloads/release/python-3123/
    
    Changelog:
    https://docs.python.org/release/3.12.3/whatsnew/changelog.html#python-3-12-3-final
    
    Binary builds:
    https://github.com/heroku/heroku-buildpack-python/actions/runs/8618946155
    
    GUS-W-14846704.

--- spec/hatchet/python_version_spec.rb
+++ spec/hatchet/python_version_spec.rb
@@ -194,7 +194,7 @@ RSpec.describe 'Python version support' do
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_11
   end
 
-  context 'when runtime.txt contains python-3.12.2' do
+  context 'when runtime.txt contains python-3.12.3' do
     let(:app) { Hatchet::Runner.new('spec/fixtures/python_3.12') }
 
     include_examples 'builds with the requested Python version', LATEST_PYTHON_3_12
