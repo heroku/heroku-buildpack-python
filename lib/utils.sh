c1042d22205cb0c5393ca4526a969c25252c4d81
Ed Morley <501702+edmorley@users.noreply.github.com>
2025-05-06 21:42:09 +0100

    Fix handling of version files that contain CRLFs (#1789)
    From looking at metrics, I noticed that the `cat --show-nonprinting`
    approach added in #1783 to aid the visualisation of invisible characters
    (such as ASCII control characters) also escapes carriage return
    characters (to `^M`), which we don't want.
    
    Instead, `sed` is now used which replaces anything not matching the
    `:print:` and `:space:` groups with the Unicode substitution character
    (`�`) - which means the classic buildpack now also matches the CNB's
    behaviour:
    https://github.com/heroku/buildpacks-python/blob/f2fdd00edf0f63b298cf88c82377885c88666440/src/python_version_file.rs#L16-L19
    
    For a mapping of what `:print:` and `:space:` cover, see the chart at
    the bottom of this page:
    https://en.cppreference.com/w/cpp/string/byte/isprint
    
    GUS-W-18225347.

--- lib/utils.sh
+++ lib/utils.sh
@@ -16,6 +16,15 @@ function utils::get_requirement_version() {
 	echo "${requirement_version}"
 }
 
+# Replaces any invisible unwanted characters (such as ASCII control codes) with the Unicode
+# replacement character, so they are visible in error messages. Also removes any carriage
+# return characters, to prevent them interfering with the rendering of error messages that
+# include the raw file contents.
+function utils::read_file_with_special_chars_substituted() {
+	local file="${1}"
+	sed --regexp-extended --expression 's/[^[:print:][:space:]]/�/g' --expression 's/\r$//' "${file}"
+}
+
 # Python bundles pip within its standard library, which we can use to install our chosen
 # pip version from PyPI, saving us from having to download the usual pip bootstrap script.
 function utils::bundled_pip_module_path() {
