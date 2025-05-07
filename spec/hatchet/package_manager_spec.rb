81696fda1576adca4072abcb9985bf55e8c9a046
Ed Morley <501702+edmorley@users.noreply.github.com>
2025-05-07 15:51:12 +0100

    Add support for the package manager uv
    Adds support for the package manager uv:
    https://docs.astral.sh/uv/
    
    And specifically uv's project mode (`uv sync`), which uses a lockfile
    rather then a `requirements.txt` file:
    https://docs.astral.sh/uv/guides/projects/
    
    (While uv does have a pip compatible mode that supports requirements
    files, using a lockfile is a best practice for deploying an app, and not
    using one means losing most of the benefits of using uv.)
    
    Apps must have a `pyproject.toml`, `uv.lock` and `.python-version` file
    (all of which are created by default by `uv init` followed by either
    `uv lock` or any of the other commands that sync/update the lockfile).
    
    We don't support `runtime.txt` (or omitting the `.python-version` file)
    when using uv, since otherwise it leads to a much worse UX in several
    cases. (For example, when the `runtime.txt` version or buildpack default
    version differs from `requires-python` in the `pyproject.toml` file. Or
    worse, when `requires-python` happens to match the distro Python in the
    underlying base image.)
    
    For now, if a `requirements.txt`, `Pipfile` or `poetry.lock` are found,
    they will take precedence over `uv.lock` for backwards compatibility
    (currently a warning is shown, but in the future this will become an error).
    This means users of the third-party `heroku-buildpack-uv` will need to
    remove that buildpack in order to use the new native uv support, since it
    exports a `requirements.txt` file during the build which will take precedence.
    
    For standard (non-Heroku CI) builds, the buildpack installs dependencies
    using `uv sync --locked --no-default-groups`, to ensure development/test
    dependencies are not installed.
    
    We don't use `--no-dev` instead, since that only excludes the `dev`
    group, and some apps may be using other group names and have added them
    to uv's `default-groups` in `pyproject.toml` so that local development
    workflows work with a plain `uv sync` with no additional args. (PEP 735
    doesn't actually prescribe recommended group names, and in fact uses
    `test` in its example rather than `dev`, so non-standard names may be
    common.)
    
    For Heroku CI builds, the buildpack installs dependencies using
    `uv sync --locked`, which will install all default groups. See:
    https://docs.astral.sh/uv/concepts/projects/dependencies/#dependency-groups
    
    For now, we don't expose user provided env vars to `uv sync` (similar to
    the behaviour for other package managers), however, will start doing so
    as part of addressing #1451 and #1700.
    
    uv is installed into the build cache rather than the slug, so is not
    available at run-time (since it's not typically needed at run-time and
    doing so reduces the slug size). Both Python and the entrypoints of
    installed dependencies are available on `PATH`, so use of `uv run` is
    not required at run-time to use dependencies in the environment.
    
    Closes #1616.
    GUS-W-17310796.

--- spec/hatchet/package_manager_spec.rb
+++ spec/hatchet/package_manager_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe 'Package manager support' do
           remote:  !     Error: Couldn't find any supported Python package manager files.
           remote:  !     
           remote:  !     A Python app on Heroku must have either a 'requirements.txt',
-          remote:  !     'Pipfile' or 'poetry.lock' package manager file in the root
-          remote:  !     directory of its source code.
+          remote:  !     'Pipfile', 'poetry.lock' or 'uv.lock' package manager file in
+          remote:  !     the root directory of its source code.
           remote:  !     
           remote:  !     Currently the root directory of your app contains:
           remote:  !     
@@ -34,10 +34,10 @@ RSpec.describe 'Package manager support' do
           remote:  !     Otherwise, add a package manager file to your app. If your app has
           remote:  !     no dependencies, then create an empty 'requirements.txt' file.
           remote:  !     
-          remote:  !     If you would like to see support for the package manager uv,
-          remote:  !     please vote and comment on these GitHub issues:
-          remote:  !     https://github.com/heroku/heroku-buildpack-python/issues/1616
-          remote:  !     https://github.com/heroku/roadmap/issues/323
+          remote:  !     If you aren't sure which package manager to use, we recommend
+          remote:  !     trying uv, since it supports lockfiles, is extremely fast, and
+          remote:  !     is actively maintained by a full-time team:
+          remote:  !     https://docs.astral.sh/uv/
           remote:  !     
           remote:  !     For help with using Python on Heroku, see:
           remote:  !     https://devcenter.heroku.com/articles/getting-started-with-python
@@ -87,6 +87,7 @@ RSpec.describe 'Package manager support' do
           remote:  !     Pipfile.lock \\(Pipenv\\)
           remote:  !     requirements.txt \\(pip\\)
           remote:  !     poetry.lock \\(Poetry\\)
+          remote:  !     uv.lock \\(uv\\)
           remote:  !     
           remote:  !     For now, we will build your app using the first package manager
           remote:  !     listed above, however, in the future this warning will become
@@ -94,10 +95,21 @@ RSpec.describe 'Package manager support' do
           remote:  !     
           remote:  !     Decide which package manager you want to use with your app, and
           remote:  !     then delete the file\\(s\\) and any config from the others.
+          remote:  !     
+          remote:  !     If you aren't sure which package manager to use, we recommend
+          remote:  !     trying uv, since it supports lockfiles, is extremely fast, and
+          remote:  !     is actively maintained by a full-time team:
+          remote:  !     https://docs.astral.sh/uv/
           remote: 
           remote: 
           remote:  !     Note: We recently added support for the package manager Poetry.
           remote:  !     If you are using a third-party Poetry buildpack you must remove
+          remote:  !     it, otherwise the requirements.txt file it generates will cause
+          remote:  !     the warning above.
+          remote: 
+          remote: 
+          remote:  !     Note: We recently added support for the package manager uv.
+          remote:  !     If you are using a third-party uv buildpack you must remove
           remote:  !     it, otherwise the requirements.txt file it generates will cause
           remote:  !     the warning above.
           remote: 
@@ -118,25 +130,25 @@ RSpec.describe 'Package manager support' do
     it 'clears the cache before installing with the new package manager' do
       app.deploy do |app|
         FileUtils.rm('requirements.txt')
-        FileUtils.cp(FIXTURE_DIR.join('poetry_basic/pyproject.toml'), '.')
-        FileUtils.cp(FIXTURE_DIR.join('poetry_basic/poetry.lock'), '.')
+        FileUtils.cp(FIXTURE_DIR.join('uv_basic/pyproject.toml'), '.')
+        FileUtils.cp(FIXTURE_DIR.join('uv_basic/uv.lock'), '.')
         app.commit!
         app.push!
-        expect(clean_output(app.output)).to include(<<~OUTPUT)
+        expect(clean_output(app.output)).to match(Regexp.new(<<~REGEX))
           remote: -----> Python app detected
           remote: -----> Using Python #{DEFAULT_PYTHON_MAJOR_VERSION} specified in .python-version
           remote: -----> Discarding cache since:
-          remote:        - The package manager has changed from pip to poetry
+          remote:        - The package manager has changed from pip to uv
           remote: -----> Installing Python #{DEFAULT_PYTHON_FULL_VERSION}
-          remote: -----> Installing Poetry #{POETRY_VERSION}
-          remote: -----> Installing dependencies using 'poetry sync --only main'
-          remote:        Installing dependencies from lock file
-          remote:        
-          remote:        Package operations: 1 install, 0 updates, 0 removals
-          remote:        
-          remote:          - Installing typing-extensions (4.12.2)
+          remote: -----> Installing uv #{UV_VERSION}
+          remote: -----> Installing dependencies using 'uv sync --locked --no-default-groups'
+          remote:        Resolved 7 packages in .+s
+          remote:        Prepared 1 package in .+s
+          remote:        Installed 1 package in .+s
+          remote:        Bytecode compiled 1 file in .+s
+          remote:         \\+ typing-extensions==4.13.2
           remote: -----> Discovering process types
-        OUTPUT
+        REGEX
       end
     end
   end
