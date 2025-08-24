69a41dec588ad5ae4cdbf911159f44f91ab43441
Ed Morley <501702+edmorley@users.noreply.github.com>
2025-08-24 13:48:15 +0100

    Fix remaining shellcheck SC2250 instances
    https://www.shellcheck.net/wiki/SC2250
    
    GUS-W-19413416.

--- vendor/buildpack-stdlib_v8.sh
+++ vendor/buildpack-stdlib_v8.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154 # TODO: Env var is referenced but not assigned.
-# shellcheck disable=SC2250 # TODO: Use braces around variable references even when not strictly required.
 
 set -euo pipefail
 
@@ -14,46 +13,46 @@ set -euo pipefail
 # NOTICE: Expects PROFILE_PATH & EXPORT_PATH to be set!
 set_env() {
 	# TODO: automatically create profile path directory if it doesn't exist.
-	echo "export $1=$2" >>"$PROFILE_PATH"
-	echo "export $1=$2" >>"$EXPORT_PATH"
+	echo "export $1=$2" >>"${PROFILE_PATH}"
+	echo "export $1=$2" >>"${EXPORT_PATH}"
 }
 
 # Usage: $ set-default-env key value
 # NOTICE: Expects PROFILE_PATH & EXPORT_PATH to be set!
 set_default_env() {
-	echo "export $1=\${$1:-$2}" >>"$PROFILE_PATH"
-	echo "export $1=\${$1:-$2}" >>"$EXPORT_PATH"
+	echo "export $1=\${$1:-$2}" >>"${PROFILE_PATH}"
+	echo "export $1=\${$1:-$2}" >>"${EXPORT_PATH}"
 }
 
 # Usage: $ un-set-env key
 # NOTICE: Expects PROFILE_PATH to be set!
 un_set_env() {
-	echo "unset $1" >>"$PROFILE_PATH"
+	echo "unset $1" >>"${PROFILE_PATH}"
 }
 
 # Usage: $ _env-blacklist pattern
 # Outputs a regex of default blacklist env vars.
 _env_blacklist() {
-	local regex=${1:-''}
-	if [[ -n "$regex" ]]; then
-		regex="|$regex"
+	local regex="${1:-}"
+	if [[ -n "${regex}" ]]; then
+		regex="|${regex}"
 	fi
-	echo "^(PATH|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LD_LIBRARY_PATH|PYTHONHOME$regex)$"
+	echo "^(PATH|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LD_LIBRARY_PATH|PYTHONHOME${regex})$"
 }
 
 # Usage: $ export-env ENV_DIR WHITELIST BLACKLIST
 # Exports the environment variables defined in the given directory.
 export_env() {
-	local env_dir=${1:-$ENV_DIR}
-	local whitelist=${2:-''}
+	local env_dir="${1:-${ENV_DIR}}"
+	local whitelist="${2:-}"
 	local blacklist
-	blacklist="$(_env_blacklist "$3")"
-	if [[ -d "$env_dir" ]]; then
+	blacklist="$(_env_blacklist "${3}")"
+	if [[ -d "${env_dir}" ]]; then
 		# Environment variable names won't contain characters affected by:
 		# shellcheck disable=SC2045
-		for e in $(ls "$env_dir"); do
-			echo "$e" | grep -E "$whitelist" | grep -qvE "$blacklist" \
-				&& export "$e=$(cat "$env_dir/$e")"
+		for e in $(ls "${env_dir}"); do
+			echo "${e}" | grep -E "${whitelist}" | grep -qvE "${blacklist}" \
+				&& export "${e}=$(cat "${env_dir}/${e}")"
 			:
 		done
 	fi
@@ -68,8 +67,8 @@ export_env() {
 sub_env() {
 	(
 		# TODO: Fix https://github.com/heroku/buildpack-stdlib/issues/37
-		export_env "$ENV_DIR" "${WHITELIST:-}" "${BLACKLIST:-}"
+		export_env "${ENV_DIR}" "${WHITELIST:-}" "${BLACKLIST:-}"
 
-		"$@"
+		"${@}"
 	)
 }
